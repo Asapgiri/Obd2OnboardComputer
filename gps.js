ad1f07e9c7d92bc0034ff3ab0f2a25a9b4e1eb91
Asapgiri <razzero29@gmail.com>
2021-11-21 11:09:29 +0100

    gps.js crash fixed.

--- gps.js
+++ gps.js
@@ -4,10 +4,18 @@ const port = new SerialPort('/dev/ttyACM0');
 const parser = new Readline();
 port.pipe(parser);
 
+const { exec } = require("child_process");
+
 const GPS = require('gps');
 const gps = new GPS;
+let firstTime = true;
+
 
 gps.on('data', function(data) {
+  if (firstTime) {
+    firstTime = false;
+    exec(`sudo date -s "${data.time}"`);
+  }
   if (data.time)
     console.log(JSON.stringify(data))//, gps.state);
 });
